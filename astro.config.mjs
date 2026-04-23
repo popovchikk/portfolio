d35c9270795962ebc2cdc573205c5a2e1dfc9c8d
popovchikk <mazai2006@bk.ru>
2026-04-23 16:39:45 +0600

    update astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,8 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
+  site: 'https://popovchikk.ru',
+  base: '/',
   integrations: [tailwind()],
-  base: '/',  // ← ИЗМЕНИ НА СЛЭШ (было '/portfolio')
   output: 'static',
 });
